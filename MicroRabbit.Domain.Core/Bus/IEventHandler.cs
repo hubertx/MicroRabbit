c5ec0bce2d7f57549f9e701cd74443f8dd56c597
hubertx@gmail.com <walnut1>
2020-10-12 16:18:10 -0400

    * Event.cs: * IEventBus.cs: * Command.cs: * IEventHandler.cs:
    * Message.cs: 3rd

---
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TEvent"></typeparam>
+    public interface IEventHandler<in TEvent>: IAsyncDisposable, IEventHandler
+        where TEvent: Event
+    {
+        Task handle(TEvent @event);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IEventHandler
+    {
+    }
+}
---
+++ MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MicroRabbit.Domain.Core.Events
+{
+    public class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+        protected Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
